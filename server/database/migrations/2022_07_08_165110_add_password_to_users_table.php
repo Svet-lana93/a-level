40f86fd1573848bba86321e47eb4f0f1cd5ee476
Svetlana Shmidt <svetashmidt93@gmail.com>
2022-07-14 15:45:48 +0300

    Default value from password was removed

--- server/database/migrations/2022_07_08_165110_add_password_to_users_table.php
+++ server/database/migrations/2022_07_08_165110_add_password_to_users_table.php
@@ -14,7 +14,7 @@ class AddPasswordToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('password')->default('hgfuyrsres');
+            $table->string('password');
         });
     }
 
