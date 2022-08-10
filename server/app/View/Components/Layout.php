1f0a5ececb4e47fea50b52bfe6496b71eb388786
Svetlana Shmidt <svetashmidt93@gmail.com>
2022-08-10 14:54:05 +0300

    Menu template was changed

--- server/app/View/Components/Layout.php
+++ server/app/View/Components/Layout.php
@@ -4,26 +4,31 @@ namespace App\View\Components;
 
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Route;
 use Illuminate\View\Component;
 use phpDocumentor\Reflection\Types\Collection;
 
 class Layout extends Component
 {
-    public $menu;
+    //public $menu;
+    public $route;
 
     /**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Route $route)
     {
+        $this->route = $route::currentRouteName();
+        /*
         $this->menu = collect(config('menu'))->map(
             function ($item) {
                 $item['active'] = request()->routeIs($item['route']);
                 return $item;
             }
         );
+        */
     }
 
     /**
--- server/resources/views/components/layout.blade.php
+++ server/resources/views/components/layout.blade.php
@@ -16,12 +16,17 @@
     </head>
     <body>
         <div>
-            @foreach($menu as $item)
-                <a href="{{ route($item['route']) }}"
-                   @if($item['active']) class="bold" @endif>
-                    {{ $item['title'] }}
-                </a>
-            @endforeach
+{{--            @foreach($menu as $item)--}}
+{{--                <a href="{{ route($item['route']) }}"--}}
+{{--                   @if($item['active']) class="bold" @endif>--}}
+{{--                    {{ $item['title'] }}--}}
+{{--                </a>--}}
+{{--            @endforeach--}}
+            <a href="{{ route('mainPage') }}" @if($route == 'mainPage') class="bold" @endif>Main page</a>
+            <a href="{{ route('books.list') }}" @if($route == 'books.list') class="bold" @endif>All books</a>
+            <a href="{{ route('users.getList') }}" @if($route == 'users.getList') class="bold" @endif>All users</a>
+            <a href="{{ route('videos.getList') }}" @if($route == 'videos.getList') class="bold" @endif>All videos</a>
+            <a href="{{ route('libraries.index') }}" @if($route == 'libraries.index') class="bold" @endif>All libraries</a>
         </div>
 
         <h1>{{ $title }}</h1>
